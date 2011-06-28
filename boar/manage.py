4808cc5cd7ce41e0d3539835d3f3452d7ff16ec1
Ben Firshman <ben@firshman.co.uk>
2011-06-28 15:33:41 +0100

    Cleaned up manage.py

--- boar/manage.py
+++ boar/manage.py
@@ -1,20 +1,8 @@
 #!/usr/bin/env python
 import os
 import site
-import sys
-
-prev_sys_path = list(sys.path) 
 
 site.addsitedir(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
-site.addsitedir(os.path.abspath(os.path.join(os.path.dirname(__file__), '../boar_ve/lib/python%s/site-packages' % '.'.join((unicode(x) for x in sys.version_info[0:2])))))
-
-# Reorder sys.path so new directories at the front.
-new_sys_path = [] 
-for item in list(sys.path): 
-    if item not in prev_sys_path: 
-        new_sys_path.append(item) 
-        sys.path.remove(item) 
-sys.path[:0] = new_sys_path
 
 from django.core.management import execute_manager
 try:
