190845701598da58fd39e90616f349edc08715b1
Ben Firshman <ben@firshman.co.uk>
2011-06-29 08:06:13 +0100

    Removed manage.py cruft

--- boar/manage.py
+++ boar/manage.py
@@ -5,12 +5,8 @@ import site
 site.addsitedir(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
 from django.core.management import execute_manager
-try:
-    import settings
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
+import settings
 
 if __name__ == "__main__":
     execute_manager(settings)
+
